69dd371f6363ba174bbecc1c19c69c53039d9268
IchiroKawashima <kawashima.ichiro172@mail.kyutech.jp>
2020-05-29 22:17:20 +0000

    modified the export list

--- src/Clash/Prelude/Extra.hs
+++ src/Clash/Prelude/Extra.hs
@@ -1,14 +1,10 @@
 module Clash.Prelude.Extra
-    ( module Clash.Prelude.DataFlow.Extra
-    , module Clash.Sized.Index.Extra
-    , module Clash.Sized.Fixed.Extra
-    , module Clash.Sized.Vector.Extra
-    , module Clash.Promoted.Nat.Extra
+    ( module Exports
     )
 where
 
-import           Clash.Prelude.DataFlow.Extra
-import           Clash.Sized.Index.Extra
-import           Clash.Sized.Fixed.Extra
-import           Clash.Sized.Vector.Extra
-import           Clash.Promoted.Nat.Extra
+import           Clash.Prelude.DataFlow.Extra  as Exports
+import           Clash.Sized.Index.Extra       as Exports
+import           Clash.Sized.Fixed.Extra       as Exports
+import           Clash.Sized.Vector.Extra      as Exports
+import           Clash.Promoted.Nat.Extra      as Exports
